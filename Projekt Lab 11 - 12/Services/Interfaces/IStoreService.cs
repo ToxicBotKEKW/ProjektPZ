8c5e418c99f312774dabef51c9a6bb84914ce5f3
ToxicBotKEKW <136969082+ToxicBotKEKW@users.noreply.github.com>
2025-05-22 20:34:17 +0200

    aa

--- Projekt Lab 11 - 12/Services/Interfaces/IStoreService.cs
+++ Projekt Lab 11 - 12/Services/Interfaces/IStoreService.cs
@@ -4,7 +4,7 @@ namespace Projekt_Lab_11___12.Services.Interfaces
 {
     public interface IStoreService
     {
-        public Task<StoreViewModel> StoreViewModel(int page = 1, int pageSize = 1, string searchQuery = "", string sortOrder = "");
+        public Task<StoreViewModel> StoreViewModel(int page = 1, int pageSize = 3, string searchQuery = "", string sortOrder = "");
         public Task<(bool Succes, String Message)> Buy(int pickaxeShopId);
     }
 }
